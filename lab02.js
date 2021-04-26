7830f4e9ee94091d4e29aac8bf05e8bc0fd9c92a
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-04-26 16:33:09 +0300

    ask about healthy food

---
+++ lab02.js
@@ -0,0 +1,35 @@
+let name = prompt('please enter my first name').toLowerCase();
+console.log(name)
+
+
+while (name !== 'mohammad') {
+
+       name = prompt('please enter my first name')
+    } 
+
+
+
+
+let foodtype = prompt('do i like healthy food ? yes or no')
+
+if (foodtype == 'yes'){
+
+    alert ("no i don't ")
+}else if(foodtype == 'no') {
+
+alert ("that's right"  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

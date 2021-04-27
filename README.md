f877d514517a5f9c06e389f46efeebd31a572089
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-04-27 16:28:54 +0300

    read

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # lab02
+
+
+<!-- modify your README.md file to describe your project with the new features you are adding. -->
+
+
+
+
+
+
+
+
+
+
+
+iam mohammad lababneh . i was born and live in irbid .
+i have studied mechanical engineering at JUST university, and i graduated in 2020.
+i was a prodction engineer in BOSRA company. after 9 monthes i quit becouse i dont like this type of work.
+i really like to do what i love. i am a motivation person who always looking foor a challengs to improve and inhance my skiles. 
+
--- lab02.js
+++ lab02.js
@@ -1,67 +1,67 @@
 
-let username =prompt('please enter your name ')
-alert ('welcom '+ username);
+// let username =prompt('please enter your name ')
+// alert ('welcom '+ username);
 
-let name = prompt('please enter my first name').toLowerCase();
-console.log(name)
-
-
-while (name !== 'mohammad') {
-
-       name = prompt('please enter my first name')
-    } 
+// let name = prompt('please enter my first name').toLowerCase();
+// console.log(name)
 
 
+// while (name !== 'mohammad') {
+
+//        name = prompt('please enter my first name')
+//     } 
 
 
-let foodtype = prompt('do i like healthy food ? yes or no'.toLowerCase())
-
-if (foodtype == ('yes')){
-
-    alert ("no i don't ")
-}
-
-else if (foodtype == ('y')){
-
-    alert ("no i don't ")}
-
-else if(foodtype == ('no')) {
-
-alert ("that's right"  )
-}
-
-else if(foodtype == ('n')) {
-
-    alert ("that's right"  )
-    }
 
 
+// let foodtype = prompt('do i like healthy food ? yes or no'.toLowerCase())
+
+// if (foodtype == ('yes')){
+
+//     alert ("no i don't ")
+// }
+
+// else if (foodtype == ('y')){
+
+//     alert ("no i don't ")}
+
+// else if(foodtype == ('no')) {
+
+// alert ("that's right"  )
+// }
+
+// else if(foodtype == ('n')) {
+
+//     alert ("that's right"  )
+//     }
 
 
 
 
 
 
-let status = prompt('AM I single or married ? '.toLowerCase())
-
-if (status == 'single'){
-
-    alert ("yes i am  ")
-}else if(status == ('married')) {
-
-alert ("no i don't "  )
-}
 
 
+// let status = prompt('AM I single or married ? '.toLowerCase())
 
-let age = prompt('How old am I ? '.toLowerCase())
+// if (status == 'single'){
 
-if (age == 24 ){
+//     alert ("yes i am  ")
+// }else if(status == ('married')) {
 
-    alert (" yes ")
-}else if(age !== 24) {
+// alert ("no i don't "  )
+// }
 
-alert ("no i don't "  )}
+
+
+// let age = prompt('How old am I ? '.toLowerCase())
+
+// if (age == 24 ){
+
+//     alert (" yes ")
+// }else if(age !== 24) {
+
+// alert ("no i don't "  )}
     
 
 
@@ -71,57 +71,57 @@ alert ("no i don't "  )}
 
 
 
-let masaf = prompt('do i like mansaf yes or no ? '.toLowerCase())
+// let masaf = prompt('do i like mansaf yes or no ? '.toLowerCase())
 
-if (masaf === ('no')){
+// if (masaf === ('no')){
 
-    alert (" no of course i like it  ")
-}
-else if (masaf == ('n')){
+//     alert (" no of course i like it  ")
+// }
+// else if (masaf == ('n')){
 
-    alert (" no of course i like it  ")
-}
-else if(masaf == ('yes')) {
+//     alert (" no of course i like it  ")
+// }
+// else if(masaf == ('yes')) {
 
-alert ("of course "  )
-}
+// alert ("of course "  )
+// }
 
-else if(masaf == ('yes')) {
+// else if(masaf == ('yes')) {
 
-alert ("of course "  )
-}
-
-
+// alert ("of course "  )
+// }
 
 
 
 
 
 
-let sport = prompt('do i like sport yes or no ? '.toLowerCase())
-
-if (sport === 'yes'){
-
-    alert ("yes i am  ")
-}
-
-else if (sport == 'y'){
-
-    alert ("yes i am  ")
-}
 
 
-else if(sport === ('no')) {
+// let sport = prompt('do i like sport yes or no ? '.toLowerCase())
 
-alert ("no i like it "  )
-}
+// if (sport === 'yes'){
 
-else if(sport == ('n')) {
+//     alert ("yes i am  ")
+// }
 
-    alert ("no i like it "  )
-    }
+// else if (sport == 'y'){
+
+//     alert ("yes i am  ")
+// }
+
+
+// else if(sport === ('no')) {
+
+// alert ("no i like it "  )
+// }
+
+// else if(sport == ('n')) {
+
+//     alert ("no i like it "  )
+//     }
 
 
 
 
-    alert ('goodbay '+ username);
+//     alert ('goodbay '+ username);

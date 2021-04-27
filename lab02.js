08e849d5938b341122cd97f7a7e084772c71a7cf
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-04-28 02:52:14 +0300

    re

--- lab02.js
+++ lab02.js
@@ -1,195 +1,202 @@
+'use strict'
+let score = 0;
+let username =prompt('please enter your name ')
+alert ('welcom '+ username);
 
-// let username =prompt('please enter your name ')
-// alert ('welcom '+ username);
+let name = prompt('please enter my first name').toLowerCase();
+console.log(name)
 
-// let name = prompt('please enter my first name').toLowerCase();
-// console.log(name)
 
+if (name == 'mohammad') {
+alert ('thats true ');
+score++;
+       } 
 
-// while (name !== 'mohammad') {
 
-//        name = prompt('please enter my first name')
-//     } 
 
 
+let foodtype = prompt('do i like healthy food ? yes or no'.toLowerCase())
 
+if (foodtype == ('yes')){
 
-// let foodtype = prompt('do i like healthy food ? yes or no'.toLowerCase())
+    alert ("no i don't ")
+}
 
-// if (foodtype == ('yes')){
+else if (foodtype == ('y')){
 
-//     alert ("no i don't ")
-// }
+    alert ("no i don't ")}
 
-// else if (foodtype == ('y')){
+else if(foodtype == ('no')) {
+    score++;
+alert ("that's right"  )
+}
 
-//     alert ("no i don't ")}
-
-// else if(foodtype == ('no')) {
-
-// alert ("that's right"  )
-// }
-
-// else if(foodtype == ('n')) {
-
-//     alert ("that's right"  )
-//     }
-
-
-
-
-
-
-
-
-// let status = prompt('AM I single or married ? '.toLowerCase())
-
-// if (status == 'single'){
-
-//     alert ("yes i am  ")
-// }else if(status == ('married')) {
-
-// alert ("no i don't "  )
-// }
-
-
-
-// let age = prompt('How old am I ? '.toLowerCase())
-
-// if (age == 24 ){
-
-//     alert (" yes ")
-// }else if(age !== 24) {
-
-// alert ("no i don't "  )}
-    
-
-
-
-
-
-
-
-
-// let masaf = prompt('do i like mansaf yes or no ? '.toLowerCase())
-
-// if (masaf === ('no')){
-
-//     alert (" no of course i like it  ")
-// }
-// else if (masaf == ('n')){
-
-//     alert (" no of course i like it  ")
-// }
-// else if(masaf == ('yes')) {
-
-// alert ("of course "  )
-// }
-
-// else if(masaf == ('yes')) {
-
-// alert ("of course "  )
-// }
-
-
-
-
-
-
-
-
-// let sport = prompt('do i like sport yes or no ? '.toLowerCase())
-
-// if (sport === 'yes'){
-
-//     alert ("yes i am  ")
-// }
-
-// else if (sport == 'y'){
-
-//     alert ("yes i am  ")
-// }
-
-
-// else if(sport === ('no')) {
-
-// alert ("no i like it "  )
-// }
-
-// else if(sport == ('n')) {
-
-//     alert ("no i like it "  )
-//     }
-
-
-
-
-//     alert ('goodbay '+ username);
-
-
-
-
-// let myAge=Number(prompt("guess my age"));
-// for(let i=0;i<4;i++){
-// if (myAge==24) { alert("correct")
-// break;
-
-// }else if (i == 3 ){
-//     alert ('the correct answer is 24')
-// break;
-// }
-
-// else if (myAge<24) {
-//     alert("too low");
-//      myAge=Number(prompt("guess my age"));
-
-
-// } else if (myAge>24){ 
-//     alert("too high");       
-// myAge=Number(prompt("guess my age"));
-
-// }
-
-// console.log (i)
-// }
-
-  
-
-
-let s =0
-
-
-let film = ['frinds', 'breaking bad', 'viking', 'preason break'];
-let ask = prompt("what is my fav film ?");
-for (let i = 1; i < 7; i++) {
-    if (film.includes(ask)) {
-        alert('yes this is one of them')
-     s = s + 1
-        
-
-        break;
-
-    } 
-
-    else {
-        alert('sorry it is not one of them')
-        ask = prompt("what is my fav film");
-        
-
+else if(foodtype == ('n')) {
+    score++;
+    alert ("that's right"  )
     }
 
 
-}    alert('the correct answers are:  ' + '  frinds  '+ '  breaking bad '+ '  viking '+ '  preason break ');
-
-alert("YOUR SCORE IS =" + s)
 
 
 
 
 
 
+let status = prompt('AM I single or married ? '.toLowerCase())
+
+if (status == 'single'){
+    score++;
+    alert ("yes i am  ")
+}else if(status == ('married')) {
+
+alert ("no i don't "  )
+}
+
+
+
+let age = prompt('How old am I ? '.toLowerCase())
+
+if (age == 24 ){
+    score++;
+    alert (" yes ")
+}else if(age !== 24) {
+
+alert ("no i don't "  )}
 
 
 
 
 
+
+
+
+
+let masaf = prompt('do i like mansaf yes or no ? '.toLowerCase())
+
+if (masaf === ('no')){
+    
+    alert (" no of course i like it  ")
+}
+else if (masaf == ('n')){
+
+    alert (" no of course i like it  ")
+}
+else if(masaf == ('yes')) {
+    score++;
+alert ("of course "  )
+}
+
+else if(masaf == ('y')) {
+    score++;
+alert ("of course "  )
+}
+
+
+
+
+
+
+
+
+let sport = prompt('do i like sport yes or no ? '.toLowerCase())
+
+if (sport === 'yes'){
+    score++;
+    alert ("yes i am  ")
+}
+
+else if (sport == 'y'){
+    score++;
+    alert ("yes i am  ")
+}
+
+
+else if(sport === ('no')) {
+
+alert ("no i like it "  )
+}
+
+else if(sport == ('n')) {
+
+    alert ("no i like it "  )
+    }
+
+
+
+
+    alert ('goodbay '+ username);
+
+
+
+
+let myAge=Number(prompt("guess my age"));
+for(let i=0;i<4;i++){
+if (myAge==24) { alert("correct")
+score++;
+break;
+
+}else if (i == 3 ){
+    alert ('the correct answer is 24')
+break;
+}
+
+else if (myAge<24) {
+    alert("too low");
+     myAge=Number(prompt("guess my age"));
+
+
+} else if (myAge>24){ 
+    alert("too high");       
+myAge=Number(prompt("guess my age"));
+
+}
+
+console.log (i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let Q7 = ['freinds', 'prison break', 'vikings', 'the walking dead',];
+let A7 = prompt("what is my film ?");
+let flag = false;
+for (let a = 1; a < 6; a++) {
+    for (let i = 0; i < Q7.length; i++) {
+        if (A7 === Q7[i]) {
+            alert('yes this is one of them')
+            flag = true;
+            score = score + 1;
+            break;
+        }
+    }
+    if (flag == true) {
+        break;
+    }
+    if (flag === false) {
+        alert('sorry it is not one of them')
+     
+       A7 = prompt("what is my fav film ?");
+
+
+
+   
+    }
+
+
+    
+
+}
+alert (score + 'out of 8');
+alert ('the right answers are :'+ ' prison break ' + ' freinds '+ ' vikings '+ ' the walking dead ' );
